d2f43731c6b5d835724a111606060671ad2df835
Kaitlin <kat.odonnel@gmail.com>
2012-09-22 18:34:00 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fables::Application.config.secret_token = '6f1c29ee55a4cc20adbc775dee81da743f14275409ccd166d8739a2640f620e1da8f8c7516e75dd78c72db0d5ef04d5f6785fe69bfc73918df62cf698b1ad9a6'
